255a01e811960a4518506c3aca2f8b6ff913d4a5
Andreea <andreeamusteata68@gmail.com>
2025-10-31 10:30:29 +0100

    1030

---
+++ Assets/TutorialInfo/Scripts/animali.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class animali : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPoseZ = 20;
+   
+   
+       
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                // Randomly generate animal index and spawn position
+                Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+                0, spawnPoseZ);
+                int animalIndex = Random.Range(0, animalPrefabs.Length);
+                Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            }
+        }
+
+    }
+}
